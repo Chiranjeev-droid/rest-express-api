a3293c77c32a839b1c38791e137607c35045d7e3
Chiranjeev Vishnoi <somu209e@gmail.com>
2022-11-05 18:46:55 +0530

    added put,post and delete request

--- app.js
+++ app.js
@@ -29,9 +29,17 @@ const Product = new mongoose.model("Product", ProductSchema);
 app.post("/api/v1/product/new", async (req, res) => {
   await Product.create(req.body);
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: req.body,
+  });
+});
+//Get all Products
+app.get("/api/v1/product/get", async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products, //products:products
   });
 });
 app.get("/api/v1/get", (req, res) => {
@@ -39,5 +47,43 @@ app.get("/api/v1/get", (req, res) => {
     success: "True",
   });
 });
+
+//Put Request for updating products
+app.put("/api/v1/product/:id", async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  //2nd argument => vo chij jo hm update krna chahte hai
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found in database",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    useFindAndModify: false,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    message: "Product updated Succesfully.",
+  });
+});
+//DELETE REQUEST FOR DELETING PRODUCT
+app.delete("/api/v1/product/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found in database",
+    });
+  }
+
+  await product.remove();
+  res.status(200).json({
+    status: "success",
+    message: "Product deleted Succesfully.",
+  });
+});
+
 app.listen(PORT);
 console.log(`Server is working on port ${PORT} `);
